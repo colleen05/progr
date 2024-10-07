24597917b98b775e013608985712349d4bb042ba
Colleen <cdnickerson05@gmail.com>
2024-10-07 06:05:52 -0300

    added tags

--- src/bin/progr.rs
+++ src/bin/progr.rs
@@ -5,33 +5,9 @@ use progr::prelude::*;
 fn main() -> io::Result<()> {
     println!("Hello, world!");
 
-    // let item = Item::new(
-    //     "john",
-    //     HashSet::from([
-    //         "human".into(),
-    //         "garfield owner".into(),
-    //         "invalid\ntag".into(),
-    //     ]),
-    //     "John Arbuckle",
-    // );
-    // item.write()?;
+    let tag = Tag::open("player");
 
-    //let mut item = Item::open("john")?;
-    //item.tags.insert("male".into());
-    //item.write()?;
-
-    //println!("{:?}", item);
-
-    let stage = Stage::new(
-        "todo",
-        HashSet::from(["player-movement".into(), "player-hud".into()]),
-    );
-    stage.write()?;
-
-    let stage = Stage::open("todo").unwrap();
-    stage.write()?;
-
-    println!("{:?}", stage);
+    println!("{:?}", tag);
 
     Ok(())
 }
--- src/tag.rs
+++ src/tag.rs
@@ -1,6 +1,55 @@
-use crate::prelude::*;
+use std::{
+    collections::HashSet,
+    fs::{self, File},
+    io::{self, Write},
+    path::PathBuf,
+};
 
+use crate::PROGR_PATH_PREFIX;
+
+#[derive(Debug)]
 pub struct Tag {
-    name: String,
-    info: String,
+    pub name: String,
+    pub description: String,
+}
+
+impl Tag {
+    pub fn new(name: &str, description: &str) -> Self {
+        Self {
+            name: name.into(),
+            description: description.into(),
+        }
+    }
+
+    pub fn path_from_name(name: &str) -> PathBuf {
+        [".", PROGR_PATH_PREFIX, "tags", name].iter().collect()
+    }
+
+    pub fn path(&self) -> PathBuf {
+        Self::path_from_name(&self.name)
+    }
+
+    pub fn open(name: &str) -> io::Result<Tag> {
+        let contents = fs::read_to_string(Self::path_from_name(name))?;
+        let description = contents.strip_suffix('\n').unwrap_or(&contents);
+
+        Ok(Tag::new(name, description))
+    }
+
+    pub fn write(&self) -> io::Result<()> {
+        // Prepare parent directory.
+        if let Some(parent_dir) = self.path().parent() {
+            fs::create_dir_all(parent_dir)?;
+        } else {
+            panic!("Expected parent directory.");
+        }
+
+        // Create the file.
+        let mut f = File::create(self.path())?;
+
+        f.write_all(self.description.as_bytes())?;
+        f.write_all("\n".as_bytes())?;
+
+        Ok(())
+    }
 }
